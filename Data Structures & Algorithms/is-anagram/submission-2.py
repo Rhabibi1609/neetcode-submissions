3be67eec0b737a009cb012be08e4a70ff5c005a2
Rayyan Habeeb <rayyanhabeeb0@gmail.com>
2026-05-22 21:16:17 -0400

    Bulk sync: 4 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        else:
+            setS = set(s)
+
+            for i in setS:
+                if s.count(i) != t.count(i):
+                    return False
+
+            return True
